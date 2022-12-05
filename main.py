0fd166e61b0ae77dd5fe7a951ad72f03bd27a88c
haowei <haoweif@andrew.cmu.edu>
2022-12-04 22:14:01 -0500

    initial holy trinity works

--- main.py
+++ main.py
@@ -142,16 +142,17 @@ def demo():
     ###
     # Burn pool tokens
     ###
-    # print("Burning")
-    # app_client.call(
-    #     ConstantProductAMM.burn,
-    #     pool_xfer=TransactionWithSigner(
-    #         txn=transaction.AssetTransferTxn(addr, sp, app_addr, 100, pool_token),
-    #         signer=signer,
-    #     ),
-    # )
-    # print_balances(app_id, app_addr, addr, pool_token, asset_a, asset_b)
-
+    print("Burning")
+    app_client.call(
+        ConstantProductAMM.burn,
+        range = 2,
+        pool_xfer=TransactionWithSigner(
+            txn=transaction.AssetTransferTxn(addr, sp, app_addr, 100, pool_token),
+            signer=signer,
+        ),
+        
+    )
+    print_balances(app_id, app_addr, addr, pool_token, asset_a, asset_b)
 
 def create_asset(addr, pk, unitname):
     # Get suggested params from network
